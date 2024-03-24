8197f4a639080343093f2843e8ffa2340977c98c
toufiq-sm-org <142045680+toufiq-sm-org@users.noreply.github.com>
2024-03-25 05:08:34 +0600

    cron name fixed

--- guelphhyundaicom.ts
+++ guelphhyundaicom.ts
@@ -24,7 +24,7 @@ export const data: {
             additionalHeader: [],
             contentType: null,
             method: RequestSetting_Method.GET,
-            name: 'gphyundaicom',
+            name: 'guelphhyundaicom',
             postData: null,
             proxyConfig: 'default',
             referrer: null,
@@ -34,7 +34,7 @@ export const data: {
         },
     ],
     config: {
-        name: 'gphyundaicom',
+        name: 'guelphhyundaicom',
         active: true,
         organization: {
             id: 177,
